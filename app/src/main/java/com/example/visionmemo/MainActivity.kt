80aea47e66edb070af13af7b6d99341376e0cfd6
Devajit <patar1447@gmail.com>
2023-08-26 22:08:43 +0530

    RecyclerView Added

--- app/src/main/java/com/example/visionmemo/MainActivity.kt
+++ app/src/main/java/com/example/visionmemo/MainActivity.kt
@@ -7,7 +7,9 @@ import android.widget.TextView
 import androidx.annotation.DrawableRes
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.visionmemo.activities.InsertNoteActivity
+import com.example.visionmemo.adapter.NotesAdapter
 import com.example.visionmemo.databinding.ActivityMainBinding
 import com.example.visionmemo.db.NotesDB
 import com.example.visionmemo.repository.NotesRepository
@@ -17,23 +19,24 @@ import com.example.visionmemo.viewmodelFactory.NotesViewModelFactory
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    lateinit var mainViewModel: NotesViewModel
+    private lateinit var mainViewModel: NotesViewModel
+    private lateinit var adapter:NotesAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
-
         val dao=NotesDB.getDatabase(this).notesDao()
         val repository=NotesRepository(dao)
-//        mainViewModel=ViewModelProvider(this,NotesViewModelFactory(repository))[NotesViewModel::class.java]
-//
-//        val data=findViewById<TextView>(R.id.txt)
-//
-//        mainViewModel.getAllNotes().observe(this, Observer {
-//            data.text=it.toString()
-//        })
+        mainViewModel=ViewModelProvider(this,NotesViewModelFactory(repository))[NotesViewModel::class.java]
+
+
+        mainViewModel.getAllNotes().observe(this, Observer {
+            binding.notesRecyclerView.layoutManager=GridLayoutManager(this,2)
+            adapter=NotesAdapter(this,it)
+            binding.notesRecyclerView.adapter=adapter
+        })
 
 
 
---
+++ app/src/main/java/com/example/visionmemo/adapter/NotesAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.visionmemo.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.visionmemo.R
+import com.example.visionmemo.model.NoteEntity
+
+
+class NotesAdapter(private var context: Context, private var list: List<NoteEntity>):RecyclerView.Adapter<NotesAdapter.NotesViewHolder>() {
+
+    inner class NotesViewHolder(private var view:View):RecyclerView.ViewHolder(view){
+        var title=view.findViewById<TextView>(R.id.txtTitle)
+        var subTitle=view.findViewById<TextView>(R.id.txtSubtitle)
+        var date=view.findViewById<TextView>(R.id.txtDate)
+        var priority=view.findViewById<View>(R.id.priorityView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotesViewHolder {
+        return  NotesViewHolder(LayoutInflater.from(context.applicationContext).inflate(R.layout.item_notes,parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: NotesViewHolder, position: Int) {
+        val notes=list[position]
+
+        holder.title.text=notes.notesTitle
+        holder.subTitle.text=notes.notesSubTitle
+        holder.date.text=notes.notesDate.toString()
+
+        if(notes.notesPriority.equals("1")){
+            holder.priority.setBackgroundResource(R.drawable.red_shape)
+        }else if(notes.notesPriority.equals("2")){
+            holder.priority.setBackgroundResource(R.drawable.blue_shape)
+        }else{
+            holder.priority.setBackgroundResource(R.drawable.yellow_shape)
+        }
+
+    }
+}
