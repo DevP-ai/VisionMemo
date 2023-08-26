56de55d830884ab255e8c29db355bac8da44970c
Devajit <patar1447@gmail.com>
2023-08-26 17:58:20 +0530

    Notes Creating all value insertion complete

--- app/src/main/java/com/example/visionmemo/activities/InsertNoteActivity.kt
+++ app/src/main/java/com/example/visionmemo/activities/InsertNoteActivity.kt
@@ -19,7 +19,7 @@ import java.util.Date
 class InsertNoteActivity : AppCompatActivity() {
     private lateinit var binding:ActivityInsertNoteBinding
     private lateinit var notesViewModel: NotesViewModel
-
+    private var priority:String="1"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityInsertNoteBinding.inflate(layoutInflater)
@@ -29,19 +29,44 @@ class InsertNoteActivity : AppCompatActivity() {
         var repo=NotesRepository(dao)
         notesViewModel=ViewModelProvider(this,NotesViewModelFactory(repo))[NotesViewModel::class.java]
 
+
+        binding.redPriority.setOnClickListener {
+            priority="1"
+            binding.redPriority.setImageResource(R.drawable.ic_check)
+            binding.bluePriority.setImageResource(0)
+            binding.yellowPriority.setImageResource(0)
+        }
+
+        binding.bluePriority.setOnClickListener {
+            priority="2"
+            binding.bluePriority.setImageResource(R.drawable.ic_check)
+            binding.redPriority.setImageResource(0)
+            binding.yellowPriority.setImageResource(0)
+        }
+
+        binding.yellowPriority.setOnClickListener {
+            priority="3"
+            binding.yellowPriority.setImageResource(R.drawable.ic_check)
+            binding.redPriority.setImageResource(0)
+            binding.bluePriority.setImageResource(0)
+        }
+
+
         binding.updateBtn.setOnClickListener {
             val title=binding.notesTitle.text.toString()
             val subTitle=binding.notesSubtitle.text.toString()
             val notes=binding.notesText.text.toString()
 
             lifecycleScope.launch {
-                validateData(title,subTitle,notes)
+                validateData(title,subTitle,notes,priority)
             }
         }
 
+
+
     }
 
-    private suspend fun validateData(title: String, subTitle: String, notes: String) {
+    private suspend fun validateData(title: String, subTitle: String, notes: String,priority:String) {
          if(title.isBlank()){
              binding.notesTitle.error="Title Required"
          }else if(subTitle.isBlank()){
@@ -49,15 +74,15 @@ class InsertNoteActivity : AppCompatActivity() {
          }else if(notes.isBlank()){
              binding.notesText.error="Please Enter notes"
          }else{
-             uploadNotes(title,subTitle,notes)
+             uploadNotes(title,subTitle,notes,priority)
              binding.notesTitle.setText("")
              binding.notesSubtitle.setText("")
              binding.notesText.setText("")
          }
     }
 
-    private suspend fun uploadNotes(title: String, subTitle: String, notes: String) {
-         notesViewModel.insertNotes(NoteEntity(0,title,subTitle,notes,Date()))
+    private suspend fun uploadNotes(title: String, subTitle: String, notes: String,priority:String) {
+         notesViewModel.insertNotes(NoteEntity(0,title,subTitle,notes,Date(),priority))
 
         Toast.makeText(this,"Notes Uploaded",Toast.LENGTH_LONG).show()
 
